17ec3164c111e8731f614b4f192334b1f2b3492b
Suprio Paul <2105085@ugrad.cse.buet.ac.bd>
2026-08-27 03:22:29 +0600

    update dependencies and simplify PyG installation in setup script

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ set -e
 
 pip install torch==2.6.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 pip install torch-geometric
-pip install torch-scatter torch-sparse torch-cluster torch-spline-conv -f https://data.pyg.org/whl/torch-2.6.0+cu124.html --no-build-isolation
+pip install torch-scatter -f https://data.pyg.org/whl/torch-2.6.0+cu124.html
 pip install scikit-learn==1.6.1 scipy==1.13.1 networkx==3.2.1 numpy==2.0.1
